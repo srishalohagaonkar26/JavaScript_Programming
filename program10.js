04702326dab2d6704a93390219228a11d3054296
SRISHA <lohagaonkarsrisha@gmail.com>
2026-08-11 14:06:00 +0530

    adding files

---
+++ program10.js
@@ -0,0 +1,41 @@
+const readline = require("readline");
+
+function CheckEvenOdd(iNo) 
+{
+    let iRemainder = 0;
+
+    if (iNo % 2 === 0) 
+    {
+        return true;  // NEW: returns Boolean, not number
+    } 
+    else 
+    {
+        return false;  // NEW: returns Boolean, not number
+    }
+}
+
+const rl = readline.createInterface
+(
+    {
+        input: process.stdin,
+        output: process.stdout
+    }
+);
+
+rl.question("Enter a number: ", (input) => {
+    let iValue = Number(input);
+
+    let bRet = CheckEvenOdd(iValue);  // NEW: stores Boolean result in bRet variable
+
+    if (bRet === true) 
+    {  
+        // NEW: checks if Boolean is true
+        console.log(iValue + " is even");
+    } 
+    else 
+    {
+        console.log(iValue + " is odd");
+    }
+
+    rl.close();
+});

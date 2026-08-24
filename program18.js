dec3f3012b085a218938f51d9dbdb19dde28af8e
SRISHA <lohagaonkarsrisha@gmail.com>
2026-08-24 18:26:14 +0530

    adding logical codes of node js

---
+++ program18.js
@@ -0,0 +1,45 @@
+const readlineSync = require("readline-sync");
+
+function CalculateTicketPrice(iAge) 
+{
+    //Input Updater
+    if(iAge < 0)
+    {
+        return -1;
+    }
+    
+    if (iAge >= 0 && iAge <= 5) 
+    {
+        return 0;
+    } 
+    else if (iAge >= 6 && iAge <= 18) 
+    {
+        return 500;
+    } 
+    else if (iAge >= 19 && iAge <= 50) 
+    {
+        return 900;
+    } 
+    else 
+    {
+        return 400;
+    }
+}
+
+let iValue = 0;
+
+iValue = readlineSync.question("Please enter your age to calculate ticket price : ");
+//  ithe readline sync nahi lihila tar code ithe input ghyayla nahi thambat
+
+let iRet = CalculateTicketPrice(iValue);
+
+console.log("Your ticket price will be " + iRet + " rupees");
+
+if(iRet === -1)
+{
+    console.log("Please enter positive age gng ;-;\n");
+}
+else
+{
+    console.log("Your ticket price will be " +iRet+ "rupees\n");
+}

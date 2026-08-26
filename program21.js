8f37476dcaa49c0cc460fd68a1fe15a3ae42e4e5
SRISHA <lohagaonkarsrisha@gmail.com>
2026-08-26 14:10:11 +0530

    adding files

---
+++ program21.js
@@ -0,0 +1,6 @@
+let iCnt = 0;
+
+for(iCnt = 0; iCnt <= 4; iCnt++)
+{
+    console.log("Jay Ganesh.....\n");
+}
---
+++ program22.js
@@ -0,0 +1,6 @@
+let iCnt = 0;
+
+for(iCnt = 0; iCnt <= 11; iCnt++)
+{
+    console.log("Jay Ganesh.....\n");
+}

8ea1be5f199fba3fe75512fefad119f7b73f89d4
SRISHA <lohagaonkarsrisha@gmail.com>
2026-09-04 14:17:33 +0530

    adding files

---
+++ program28.js
@@ -0,0 +1,25 @@
+const readlineSync = require("readline-sync");
+
+function Display(iNo)
+{
+    let iCnt = 0;
+
+    //filter
+    if(iNo < 0)
+    {
+        console.log("Invalid input");
+        return;
+    }
+
+    for(iCnt = 0; iCnt <= iNo; iCnt++)
+    {
+        console.log(iCnt);
+    }
+}
+
+let iValue = 0;
+
+iValue = readlineSync.question("Enter the frequency : \n");
+iValue = Number(iValue);    //this line converts string to number as if we give an input as 5 it takes the input itself as "5" and not a number 5  so this is imp
+
+Display(iValue);
